2f2a745aeef8a75cb35b55659807f7b4146f4fe3
Kh4wf0x <118027978+SANTOOS00@users.noreply.github.com>
2025-10-24 18:52:11 +0000

    libft

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: moerrais <moerrais@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/23 13:29:43 by moerrais          #+#    #+#             */
-/*   Updated: 2025/10/23 17:24:09 by moerrais         ###   ########.fr       */
+/*   Created: 2025/10/24 17:02:26 by moerrais          #+#    #+#             */
+/*   Updated: 2025/10/24 19:50:07 by moerrais         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,11 @@
 t_list *ft_lstnew(void *content)
 {
     t_list *box;
-    box = malloc (sizeof(t_list));
-    if (box == NULL)
+
+    box = (t_list *)malloc(sizeof(t_list));
+    if (!box)
     {
-        return (NULL);
+        return (box);
     }
     box->content = content;
     box->next = NULL;
